72b645a6b87b2acfdec6d28362589a5091c42f6b
shuradym <alexander.dymnikov@gmail.com>
2015-11-01 17:08:28 +1300

    first version.

---
+++ README.txt
@@ -0,0 +1,30 @@
+
+CONTENTS OF THIS FILE
+---------------------
+
+ * Introduction
+ * Installation
+ * Credits
+
+
+INTRODUCTION
+------------
+
+ABC-Slideshow module provides quick implementation of two (at present version) JavaScript libraries
+to organize the slideshow.
+ABC-Slideshow module covers cases of mock-up front-end development, demo website or the usage at the production
+environment as the example of quick implementation of JavaScript library.
+
+
+INSTALLATION
+------------
+
+1. Copy the abcslideshow directory to your sites/SITENAME/modules directory.
+
+2. Enable the module at Administer >> Site building >> Modules.
+
+
+CREDITS
+-------
+* Alexander Dymnikov (dymale)
+
---
+++ templates/nivo_slideshow.tpl.php
@@ -0,0 +1,27 @@
+
+<!-- header image/slider -->
+<div class="header_bot header_slider">
+    <div class="container">
+
+
+        <section>
+
+            <!-- showcase slider -->
+            <div id="showcase" class="showcase">
+
+
+                <div class="slider-wrapper theme-default">
+                    <div id="nivo-slides" class="nivoSlider">
+                        <?php foreach ($showonpage['images'] as $key => $img): ?>
+                            <img src="/myd7/sites/default/files/<?php print $img; ?>" data-thumb="/myd7/sites/default/files/<?php print $img; ?>" alt="0<?php print $key; ?>" />
+                        <?php endforeach; ?>
+                    </div>
+
+                </div>
+
+            </div>
+            <!--/ showcase slider -->
+        </section>
+    </div>
+</div>
+<!--/ header image/slider -->
